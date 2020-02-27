442a49485809192adfc0d6ef09bd4e2224ab01be
Samofan <hello@jkirifuda.com>
2020-02-27 20:58:08 +0100

    Create DBUtil class and moved Web.config file

---
+++ Util/DBUtil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace ShookREST.Util
+{
+    public class DBUtil
+    {
+        public DBUtil()
+        {
+            var client = new MongoClient(
+                "");
+            var database = client.GetDatabase("test");
+
+            Console.WriteLine(database);
+        }
+    }
+}
