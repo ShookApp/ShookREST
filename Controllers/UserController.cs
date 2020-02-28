236ca432331f0eb89969dc36e65298f978877cb9
ShookApp Admin <shook.app@aol.com>
2020-02-28 11:54:47 +0100

    Rearange code

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -3,17 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ShookREST.Models;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ShookREST.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class UserController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        // Get request.
+        public User Get()
         {
-            return View();
+            return new User()
+            {
+                FirstName = "Max",
+                LastName = "Mustermann"
+            };
         }
     }
 }
